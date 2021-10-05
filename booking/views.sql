eea3faa85851e48425cea16232effb73706f7ee3
SBkhat <beyondbirthday77@gmail.com>
2021-10-06 00:00:32 +0300

    user_profile_view

--- booking/views.sql
+++ booking/views.sql
@@ -14,3 +14,21 @@ CREATE OR REPLACE VIEW orders_view AS
     INNER JOIN hotels h ON o.hotel_id = h.id
     INNER JOIN users u2 ON h.user_id = u2.id
     INNER JOIN user_phones up2 ON u2.phone_id = up2.id;
+
+
+CREATE OR REPLACE VIEW user_profile_view AS
+  SELECT
+    u.firstname,
+    u.lastname,
+    u.id,
+    upn.value as phone,
+    uph.value as photo_src,
+    g.name as gender_name,
+    us.name as status,
+    p.birthday as birthday
+  FROM users as u
+    INNER JOIN profiles p ON u.id = p.user_id
+    INNER JOIN user_phones upn ON u.phone_id = upn.id
+    INNER JOIN user_photos uph ON u.photo_id = uph.id
+    INNER JOIN user_statuses us ON u.status_id = us.id
+    INNER JOIN genders g ON p.gender_id = g.id
