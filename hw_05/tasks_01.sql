b48b3a83190e41ff0366e15989741845654b0e29
SBkhat <beyondbirthday77@gmail.com>
2021-10-20 20:12:57 +0300

    домашнее задание №5

--- hw_05/tasks_01.sql
+++ hw_05/tasks_01.sql
@@ -1,44 +1,45 @@
---Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
-create database new_database;
-use new_database;
+-- Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
+CREATE DATABASE new_database;
+
+USE new_database;
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", -- искуственный ключ
-  name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
-  created_at DATETIME DEFAULT null COMMENT "Время создания строки",
-  updated_at DATETIME DEFAULT null COMMENT "Время обновления строки"
-) COMMENT "Пользователи";
+  id SERIAL, -- искуственный ключ
+  name VARCHAR(100) NOT NULL,
+  created_at DATETIME DEFAULT NULL,
+  updated_at DATETIME DEFAULT NULL
+);
 
-INSERT INTO users (`name`) VALUES ('123');
-INSERT INTO users (`name`) VALUES ('323232');
-INSERT INTO users (`name`) VALUES ('43434');
+INSERT INTO users (`name`) VALUES ('имя 1');
+INSERT INTO users (`name`) VALUES ('имя 2');
+INSERT INTO users (`name`) VALUES ('имя 3');
 
---ставим null в датах
-UPDATE users SET updated_at = null WHERE (id = 1);
-UPDATE users SET created_at = null, updated_at = null WHERE (id = 2);
-UPDATE users SET created_at = null WHERE (id = 3);
+-- ставим null в датах
+UPDATE users SET updated_at = null WHERE id IN (1,2,3);
 
---проверяем
+-- проверяем
 SELECT * FROM new_database.users;
 
---обновляем
+-- обновляем
 UPDATE users SET updated_at = now() WHERE updated_at is null;
 UPDATE users SET created_at = now() WHERE created_at is null;
 
---проверяем
+-- проверяем
 SELECT * FROM new_database.users;
 
 
---Таблица users была неудачно спроектирована. Записи created_at и updated_at
---были заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10. Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения.
+-- Таблица users была неудачно спроектирована. Записи created_at и updated_at
+-- были заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10. Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения.
 
-create table users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
 (
-	id int unsigned not null auto_increment
-		primary key,
-	name varchar(100) not null,
-	created_at varchar(255) not null,
-	updated_at varchar(255) not null
-)
+  id SERIAL,
+  name varchar(100) not null,
+  created_at varchar(255) not null,
+  updated_at varchar(255) not null
+);
 
 INSERT INTO users (`name`, `created_at`, `updated_at`) VALUES ('1', '20.10.2017 8:10', '20.10.2017 8:10');
 INSERT INTO users (`name`, `created_at`, `updated_at`) VALUES ('2', '20.10.2017 8:10', '20.10.2017 8:10');
@@ -50,11 +51,11 @@ UPDATE users SET created_at = DATE_FORMAT(STR_TO_DATE(created_at, '%d.%m.%Y %H:%
 ALTER TABLE new_database.users MODIFY created_at DATETIME COMMENT 'Время создания строки';
 ALTER TABLE new_database.users MODIFY updated_at DATETIME COMMENT 'Время обновления строки';
 
---В таблице складских запасов storehouses_products в поле value
---могут встречаться самые разные цифры: 0, если товар закончился и выше нуля,
---если на складе имеются запасы. Необходимо отсортировать записи таким образом,
---чтобы они выводились в порядке увеличения значения value.
---Однако нулевые запасы должны выводиться в конце, после всех записей.
+-- В таблице складских запасов storehouses_products в поле value
+-- могут встречаться самые разные цифры: 0, если товар закончился и выше нуля,
+-- если на складе имеются запасы. Необходимо отсортировать записи таким образом,
+-- чтобы они выводились в порядке увеличения значения value.
+-- Однако нулевые запасы должны выводиться в конце, после всех записей.
 
 DROP TABLE IF EXISTS storehouses_products;
 CREATE TABLE storehouses_products (
@@ -78,9 +79,9 @@ ORDER BY
   ELSE value END;
 
 
---(по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае.
---Месяцы заданы в виде списка английских названий (may, august)
-
+-- (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае.
+-- Месяцы заданы в виде списка английских названий (may, august)
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) COMMENT 'Имя покупателя',
@@ -111,3 +112,11 @@ SELECT
   id
 FROM users
 WHERE (MONTH(birthday_at) = 5 or MONTH(birthday_at) = 8);
+
+/* (по желанию) Из таблицы catalogs извлекаются записи при помощи запроса. SELECT * FROM catalogs WHERE id IN (5, 1, 2);
+Отсортируйте записи в порядке, заданном в списке IN.
+*/
+SELECT *
+FROM catalogs
+WHERE id IN (5, 1, 2)
+ORDER BY FIELD(id, 5, 1, 2);
---
+++ hw_05/tasks_02.sql
@@ -0,0 +1,55 @@
+-- Подсчитайте средний возраст пользователей в таблице users
+SELECT AVG(
+           (YEAR(CURRENT_DATE)-YEAR(birthday_at))-(RIGHT(CURRENT_DATE,5) < RIGHT(birthday_at,5))
+       ) as avg_age
+FROM users;
+
+-- Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели.
+-- Следует учесть, что необходимы дни недели текущего года, а не года рождения.
+SELECT
+  name,
+  birthday_at,
+  DAYOFWEEK(
+      CONCAT(
+          YEAR(NOW()),
+          '-',
+          MONTH(birthday_at),
+          '-',
+          DAYOFMONTH(birthday_at)
+      )
+  ) as day_of_week
+FROM users;
+
+SELECT
+  COUNT(1),
+  DAYOFWEEK(
+      CONCAT(
+          YEAR(NOW()),
+          '-',
+          MONTH(birthday_at),
+          '-',
+          DAYOFMONTH(birthday_at)
+      )
+  ) as day_of_week
+FROM users
+GROUP BY day_of_week
+ORDER BY day_of_week ASC;
+-- day_of_week - дни недели. 1 = пн, 2 = вт ... 7 = вс.
+
+-- (по желанию) Подсчитайте произведение чисел в столбце таблицы
+DROP TABLE IF EXISTS some_table;
+CREATE TABLE some_table (
+  value INT NOT NULL
+);
+
+INSERT INTO new_database.some_table (value) VALUES
+  (1),
+  (2),
+  (3),
+  (4),
+  (5);
+
+SELECT EXP(
+           SUM(LOG(value))
+       ) as sum
+FROM some_table;
